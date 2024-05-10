ae5572edc75161a0e025518e0c06f746c524eeac
bekaotiashvili14 <bekaotiashvili14@gmail.com>
2024-05-10 18:11:45 +0400

    i dont know its bug

--- app/tic.js
+++ app/tic.js
@@ -15,7 +15,10 @@ container.forEach((element) => {
     }
   });
 });
-
+const savedPlayerOne = sessionStorage.getItem("name1");
+const savedPlayerTwo = sessionStorage.getItem("name2");
+playerPoint1.innerHTML = `${savedPlayerOne}  point 0`;
+playerPoint2.innerHTML = `${savedPlayerTwo}  point 0`;
 function checkForWin() {
   const items = [
     document.getElementById("items1").textContent,
@@ -50,11 +53,11 @@ function checkForWin() {
     if (items[a] && items[a] === items[b] && items[a] === items[c]) {
       if (items[a] === "X") {
         game_winner.innerHTML = `Game winner is ${savedPlayerOne} `;
-        playerPoint1.innerHTML = `${(player1Winner += 1)} `;
+        playerPoint1.innerHTML = `${savedPlayerOne} point  ${(player1Winner += 1)} `;
         resetBoard();
       } else {
-        game_winner.innerHTML = `Game winner is    ${savedPlayerTwo}  `;
-        playerPoint2.innerHTML = `${(player2Winner += 1)} `;
+        game_winner.innerHTML = `Game winner is   ${savedPlayerTwo}  `;
+        playerPoint2.innerHTML = `${savedPlayerTwo} point  ${(player2Winner += 1)} `;
         resetBoard();
       }
       return true;
